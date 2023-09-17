2852e1734ef4aa9a192af2e51fe29bef28457177
Trevor <trevortrusty@outlook.com>
2023-09-17 00:53:56 -0400

    broken react-spring/parallax implementation with section-1

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Parallax, ParallaxLayer  } from "@react-spring/parallax";
 
 import logo from './Cat.svg';
 import waves from './layered-waves-haikei.svg';
@@ -6,12 +7,12 @@ import Cat from './components/Cat.js'
 import CodeBlock from './components/CodeBlock';
 import CodeDisplay from './components/CodeDisplay';
 import laptop from './blender_laptop.png'
-import { Parallax, ParallaxLayer } from '@react-spring/parallax';
 function App() {
   return (
     <div className="App">
       
-      <div className="App-container">
+      <Parallax className="App-container" pages={1} style={{ top: '0', left: '0' }}>
+        <ParallaxLayer offset={0} speed={2.5}>
         <div className='nav'>
           <div className='nav-left'>
             <a href='http://www.google.com' className='firstName'>TREVOR</a>
@@ -22,31 +23,26 @@ function App() {
             <a className='navBtnProjects'>PORTFOLIOLI</a>
           </div>
         </div>
-        <div className='section-1'>
+          <div className='section-1'></div>
+        </ParallaxLayer>
+        <ParallaxLayer className='section-1-content' offset={0} speed={2.5}>
           <div className='flex-1'>  
-            <h1>FULL STACK <span className='developer'>DEVELOPER</span></h1>
-            <h3>ELEVATE YOUR DIGITAL WORLD</h3>
+              <h1>FULL STACK <span className='developer'>DEVELOPER</span></h1>
+              <h3>ELEVATE YOUR DIGITAL WORLD</h3>
           </div>
           <div className='flex-2'>
             <img className='laptop' width='80%' src={laptop}/>
-            {/* <CodeDisplay>//ReactJS..</CodeDisplay>
-            <CodeDisplay>//NextJS...</CodeDisplay>
-            <CodeDisplay>//Node.....</CodeDisplay>
-            <CodeDisplay>//MySQL....</CodeDisplay>
-            <CodeDisplay>//C++......</CodeDisplay> */}
-            
+              {/* <CodeDisplay>//ReactJS..</CodeDisplay>
+              <CodeDisplay>//NextJS...</CodeDisplay>
+              <CodeDisplay>//Node.....</CodeDisplay>
+              <CodeDisplay>//MySQL....</CodeDisplay>
+              <CodeDisplay>//C++......</CodeDisplay> */}
+              
           </div>
-        </div>
-        <div className='section-2'></div>
-
-
-        <div className='footer'>
-          <p>SOFTWARE SOFTWARE</p>
-          <p>TREVORTRUSTY.COM</p>
-          <p>making SOFTWARE since 1999</p>
-          <p>MAINTAINED BY BIG SOFTWARE INC</p>
-        </div>
-      </div>
+            
+        </ParallaxLayer>
+      </Parallax>
+      
     </div>
   );
 }
